bfe266152834d452d123d631370251ef329fd0de
Z3RO4l <zordzraplha@gmail.com>
2025-07-01 19:57:44 +0000

    Atibuto Vencedor

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+#include <string.h>
+
 int main (){
 
     printf("desafio das cartas - super trunfo _CARTA A01\n") ;
@@ -15,7 +17,7 @@ float Area1;
 float PIB1 ;
 float Densidade_Populacional1;
 float PIB_Percapita1;
-float SUPER_PODER1;
+
 // DADOS DA CARTA B02
 
 char Estado2 [40];
@@ -27,7 +29,7 @@ float Area2;
 float PIB2 ;
 float Densidade_Populacional2;
 float PIB_Percapita2;
-float SUPER_PODER2;
+
 
 //ENTRADA DE DADOS _ CARTAS 
 //CARTA A01
@@ -60,7 +62,7 @@ float SUPER_PODER2;
 
      PIB_Percapita1 = (float) (PIB1 + Populacao1 ) /2;
 
-     SUPER_PODER1 = ( Populacao1 + Area1 + PIB1 + PIB_Percapita1 + Ponto_Turistico1 );
+     
      
     //entrada de dados carta 2  
 
@@ -92,7 +94,7 @@ float SUPER_PODER2;
 
      PIB_Percapita2 = (float) (PIB2 + Populacao2 ) /2;
 
-     SUPER_PODER2 = ( Populacao2 + Area2 + PIB2 + PIB_Percapita2 + Pontos_Turisticos2);
+     
 
 
      //exibicao de Dados da carta 
@@ -118,7 +120,7 @@ float SUPER_PODER2;
 
      printf("PIB Peercapita: %.2f\n", PIB_Percapita1);
 
-     printf("SUPER PODER  %f\n", SUPER_PODER1);
+     
      
      printf("==============================\n");
 
@@ -144,17 +146,11 @@ float SUPER_PODER2;
 
      printf("PIB Percapita: %.2f\n", PIB_Percapita2);
 
-     printf(" SUPER PODER %f\n", SUPER_PODER2);
-    
+   
     printf("=============================\n");
 
 
      printf("\n==================COMPARAÇÃO DE CARTAS==================\n");
-
-    printf("POPULAÇÃO : %s\n" ,
-        (Populacao1 > Populacao2 ? "carta 1 venceu":  
-        (Populacao1 < Populacao2 ? "carta 2 venceu" : 
-        "Empate" )));
 
     printf("AREA : %s\n", 
         (Area1 > Area2 ? "carta 1 venceu":
@@ -176,18 +172,28 @@ float SUPER_PODER2;
         (Densidade_Populacional1 < Densidade_Populacional2 ? "carta 2 venceu" :
         "Empate" )));
 
-    printf(" PIB PER CAPTA :   %s\n", 
+    printf("PIB PER CAPTA :   %s\n", 
         (PIB_Percapita1 > PIB_Percapita2 ? "carta 1 venceu":
         (PIB_Percapita1 < PIB_Percapita2 ? " carta 2 venceu": 
         "Empate")));
 
-    printf("SUPER PODER: %s\n", 
-        (SUPER_PODER1 > SUPER_PODER2 ? " carta 1 venceu" :
-        (SUPER_PODER1 < SUPER_PODER2 ? "carta 2 venceu": 
-        "Empate")));
-
     printf("\n=====================================================\n");
+ 
+    printf("\n----------------Comparação Final----------------------\n");
+    printf("ATRIBUTO: PIB \n");
 
+    printf("CARTA 01 Manaus :\n");
+    printf("CARTA 02 Bahia  : \n");
+    printf("RESULTADO: ");
+     if (PIB1 > PIB2){
+        printf("A carta 1 VENCEU :");
+    }
+    else
+    {
+    printf("a carta 2  VENCEU:");
+    }
+printf("\n--------------------------------------\n");
+     
     return 0; 
      
 }
